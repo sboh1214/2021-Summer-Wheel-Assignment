6d226bd17b54a81e93fafe08012d4dbaaef5bc5e
Seungbin Oh <sboh1214@gmail.com>
2021-08-08 17:43:50 +0900

    Fix domain array

--- init-docker-compose.sh
+++ init-docker-compose.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains="chat.sboh.dev"
+domains=(chat.sboh.dev)
 rsa_key_size=4096
 data_path="./nginx/certbot"
 email="sboh1214@gmail.com" # Adding a valid address is strongly recommended
